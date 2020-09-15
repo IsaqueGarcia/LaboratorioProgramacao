fb8e5916e9191cf774b2529da27c4e5122238731
IsaqueGarcia <isaqueemprego1@gmail.com>
2020-09-15 17:02:09 -0300

    Carregando informações na home do app.

--- src/gui/HomeViewController.java
+++ src/gui/HomeViewController.java
@@ -1,11 +1,15 @@
 package gui;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import input.ColaboradorInput;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,8 +17,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class HomeViewController {
-	
+public class HomeViewController implements Initializable{
+		
 	@FXML
 	private Button btCadastrar;
 	
@@ -29,6 +33,8 @@ public class HomeViewController {
 	
 	@FXML
 	private Label localTrabalho;
+	
+	ColaboradorInput colaborador;
 	
 	@FXML
 	public void onBtCadastrarAction(ActionEvent event){
@@ -49,4 +55,23 @@ public class HomeViewController {
 	public void onBtSairAction(){
 		Platform.exit();
 	}
+	
+	public void carregarInformacoes(String nome,String atividade, String localTrabalho){
+		try{
+			this.nomeColaborador.setText(nome);
+			this.atividade.setText(atividade);
+			this.localTrabalho.setText(localTrabalho);
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@SuppressWarnings("static-access")
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		LoginViewController login = new LoginViewController();
+		this.carregarInformacoes(login.retornaColaborador(), login.retornaAtividade(), login.retornaLocal());
+	}
+
+	
 }
--- src/gui/LoginViewController.java
+++ src/gui/LoginViewController.java
@@ -1,7 +1,8 @@
 package gui;
 
-import java.io.IOException;
+import com.mysql.jdbc.StringUtils;
 
+import input.ColaboradorInput;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +13,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import repository.LoginRepository;
 
 
 public class LoginViewController {
@@ -24,23 +26,50 @@ public class LoginViewController {
 	
 	@FXML
 	private PasswordField inputSenha;
+
 	
+	LoginRepository repo = new LoginRepository();
+	
+	static ColaboradorInput result = new ColaboradorInput();
+
 	@FXML
 	public void onBtEnterAction(ActionEvent event){
         try {
-			Parent parent = FXMLLoader.load(getClass().getResource("Home.fxml"));
-			Scene scene = new Scene(parent);
-			Stage stage = new Stage();
-			stage.setScene(scene);
-			stage.show();
-			((Node)(event.getSource())).getScene().getWindow().hide();
+        	
+        	 result = repo.validarLogin(inputLogin.getText(), inputSenha.getText());
+        	 
+        	if(!StringUtils.isNullOrEmpty(inputLogin.getText()) && !StringUtils.isNullOrEmpty(inputSenha.getText()) ){
+        		
+            	if(result.getUsuario().equals(inputLogin.getText()) && result.getSenha().equals(inputSenha.getText())){
+        			Parent parent = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        			Scene scene = new Scene(parent);
+        			Stage stage = new Stage();
+        			stage.setScene(scene);
+        			stage.show();
+        			((Node)(event.getSource())).getScene().getWindow().hide();
+            	}
+        		
+        	}
+        	
+
+
         }
-        catch (IOException e) {
+        catch (Exception e) {
             e.printStackTrace();
         }
 	}
 	
+	public static String retornaColaborador(){
+		return result.getNomeCompleto();
+	}
 	
+	public static String retornaAtividade(){
+		return result.getAtividade();
+	}
+	
+	public static String retornaLocal(){
+		return result.getPostoTrabalho();
+	}
 
 	
 }
