e15198c6b5634c59fbb576cdda11cd5c1030cefd
Isaque Garcia <Isaque Garcia@DESKTOP-IVFKFLJ>
2020-10-25 11:59:59 -0300

    Cadastrar funcionario view, controller criada

---
+++ src/gui/CadastrarFuncionarioViewController.java
@@ -0,0 +1,69 @@
+package gui;
+
+import input.FuncionarioInput;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import repository.FuncionarioRepository;
+
+public class CadastrarFuncionarioViewController {
+
+	@FXML
+	private TextField nomeCompleto;
+	@FXML
+	private TextField email;
+	@FXML
+	private TextField telefone;
+	@FXML
+	private TextField login;
+	@FXML
+	private TextField senha;
+	@FXML
+	private Button btCadastrar;
+	@FXML
+	private ComboBox<String> diretoria;
+	@FXML
+	private ComboBox<String> tipoDeconta
+	;
+
+//	@FXML
+//	private TableView<Map> tbFuncionario;
+//	
+//	@FXML
+//	private TableColumn colId;
+//	@FXML
+//	private TableColumn colNome;
+//	@FXML
+//	private TableColumn colEmail;
+//	@FXML
+//	private TableColumn colTelefone;
+//	@FXML
+//	private TableColumn colTipoConta;
+//	@FXML
+//	private TableColumn colDiretoria;
+	
+	
+	FuncionarioRepository r = new FuncionarioRepository();
+
+	public void cadastrarColaborador(ActionEvent event) {
+		try {
+
+			FuncionarioInput input = new FuncionarioInput(null,nomeCompleto.getText(), email.getText(), telefone.getText(),
+					diretoria.getValue(),tipoDeconta.getValue(), login.getText(), senha.getText());
+			r.InserirFuncionario(input);
+			
+			nomeCompleto.setText("");
+			email.setText("");
+			telefone.setText("");
+			login.setText("");
+			senha.setText("");
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+	
+}
---
+++ src/input/FuncionarioInput.java
@@ -0,0 +1,100 @@
+package input;
+
+public class FuncionarioInput {
+
+
+	private Integer id;
+	private String nomeCompleto;
+	private String email;
+	private String telefone;
+	private String diretoria;
+	private String tipoDeConta;
+	private String login;
+	private String senha;
+	
+	public FuncionarioInput() {
+		
+	}
+	
+	public FuncionarioInput(Integer id, String nomeCompleto, String email, String telefone, String diretoria,
+			String tipoDeConta, String login, String senha) {
+		super();
+		this.id = id;
+		this.nomeCompleto = nomeCompleto;
+		this.email = email;
+		this.telefone = telefone;
+		this.diretoria = diretoria;
+		this.tipoDeConta = tipoDeConta;
+		this.login = login;
+		this.senha = senha;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getDiretoria() {
+		return diretoria;
+	}
+
+	public void setDiretoria(String diretoria) {
+		this.diretoria = diretoria;
+	}
+
+	public String getTipoDeConta() {
+		return tipoDeConta;
+	}
+
+	public void setTipoDeConta(String tipoDeConta) {
+		this.tipoDeConta = tipoDeConta;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+	
+	
+	
+}
